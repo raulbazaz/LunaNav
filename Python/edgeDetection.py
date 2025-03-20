bc253d766c881a653296d9f1adef70d3ed5e5a81
raulbazaz <112102217+raulbazaz@users.noreply.github.com>
2025-03-20 12:04:45 +0530

    added edge detection

--- Python/edgeDetection.py
+++ Python/edgeDetection.py
@@ -1,5 +1,11 @@
 import cv2
 
-
 def edgeDetection(imgname, imgpath):
-    img = cv2.imread()
+    clahe = cv2.imread(imgpath, cv2.IMREAD_GRAYSCALE)
+    edgeDetection = cv2.Canny(clahe,50,150)
+    cv2.imwrite(f"{imgname}_edgeDet.png", edgeDetection)
+    return edgeDetection
+
+edgeDetection("2D", '2D_Clahe.png')
+edgeDetection("3D", '3D_Clahe.png')
+
