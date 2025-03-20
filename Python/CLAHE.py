3debbe5eaa21120143bd1efc9bd4055867f6fdf5
raulbazaz <112102217+raulbazaz@users.noreply.github.com>
2025-03-20 11:31:53 +0530

    applied clahe_image processing

--- Python/CLAHE.py
+++ Python/CLAHE.py
@@ -1,6 +1,5 @@
 import cv2
 
-
 def claheprocessing(imgname, imgpath):
     img = cv2.imread(imgpath, cv2.IMREAD_GRAYSCALE)
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
@@ -10,6 +9,7 @@ def claheprocessing(imgname, imgpath):
 
 claheprocessing("2D", '2D_grayscaled.png')
 claheprocessing("3D",'3D_grayscaled.png')
+
 
 
 
