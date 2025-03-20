6f9ed3a648a0dcbbb3c10586aac4f15ba6617c00
raulbazaz <112102217+raulbazaz@users.noreply.github.com>
2025-03-20 14:08:32 +0530

    adding cleanNoise filter

--- Python/imageProcessing.py
+++ Python/imageProcessing.py
@@ -1,13 +1,36 @@
 import cv2
+import numpy as np
 
-def edgeDetection(imgname, imgpath):
+def edgedetection(imgname, imgpath):
     imgload = cv2.imread(imgpath, cv2.IMREAD_GRAYSCALE)
     blurred = cv2.GaussianBlur(imgload,(5,5), 0)
     edgeDetection = cv2.Canny(blurred,50,150)
     cv2.imwrite(f"{imgname}_edgeDet.png", edgeDetection)
     return edgeDetection
 
-edgeDetection("2D", '2D_Clahe.png')
-edgeDetection("3D", '3D_Clahe.png')
+def thresholding(imgname, imgpath):
+    imgload = cv2.imread(imgpath, cv2.IMREAD_GRAYSCALE)
+    thresh = cv2.adaptiveThreshold(imgload, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 11, 2)
+    cv2.imwrite(f"{imgname}_thresh.png", thresh)
+    return thresh
+
+
+# thresholding('2D', '2D_edgeDet.png')
+# thresholding('3D', '3D_edgeDet.png')
+
+def cleanNoise(imgname, imgpath):
+    imgload = cv2.imread(imgpath, cv2.IMREAD_GRAYSCALE)
+    kernel = np.ones((3, 3), np.uint8)
+    cleaned = cv2.morphologyEx(imgload, cv2.MORPH_CLOSE, kernel, iterations=2)
+    cv2.imwrite(f"{imgname}_cleaned.png", cleaned)
+    return cleaned
+
+cleanNoise('2D', '2D_thresh.png')
+cleanNoise('3D', '3D_thresh.png')
+
+
+
+
+
 
 
