3b3626f6e8ddf9eb94878c8b7fdab32a7541bafb
raulbazaz <112102217+raulbazaz@users.noreply.github.com>
2025-03-21 13:04:25 +0530

    structuring
    git

---
+++ Python/impl.py
@@ -0,0 +1,40 @@
+import cv2
+
+x_coordinates = input(input("Enter the X coordinates :"))
+y_coordinates = int(input("Enter the Y coordinates: "))
+
+goal = (y_coordinates,x_coordinates)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import cv2
+# import matplotlib.pyplot as plt
+#
+# # Load the image
+# image = cv2.imread("3D_cleaned_final.png", cv2.IMREAD_GRAYSCALE)
+#
+# # Define known pixel coordinates of the rover
+# start = (742,407)  # Replace with actual pixel coordinates
+# goal = (358,675)
+#
+# # Plot the image with the rover's start position
+# plt.imshow(image, cmap="gray")
+# plt.scatter(start[1], start[0], color='red', marker='o')  # (x, y) in Matplotlib
+# plt.scatter(goal[1], goal[0], color='blue', marker='o', label="Goal")   # Blue for goal
+# plt.show()
+
+
